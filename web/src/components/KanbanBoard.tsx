c4c25dd5e0a79be484f4c0a52b0ce4c37c08568a
FraN-arti <artur.kalish.90@gmail.com>
2026-06-12 18:08:58 +0300

    fix: import React for React.Fragment in priority selector

--- web/src/components/KanbanBoard.tsx
+++ web/src/components/KanbanBoard.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useRef } from 'react'
+import React, { useState, useEffect, useCallback, useRef } from 'react'
 import { API_BASE } from '../config'
 import { DndContext, closestCenter } from '@dnd-kit/core'
 import { SortableContext, useSortable, arrayMove, horizontalListSortingStrategy } from '@dnd-kit/sortable'
